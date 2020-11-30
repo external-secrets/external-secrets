fe86491c767d67c39ac89aeb073ce39860d2d630
Jonatas Baldin <jonatas.baldin@gmail.com>
2020-11-30 13:24:07 +0100

    Change CRD subdomain from external-secerets.x-k8s.io to external-secrets.io

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the external-secrets v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=external-secrets.x-k8s.io
+// +groupName=external-secrets.io
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "external-secrets.x-k8s.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "external-secrets.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
