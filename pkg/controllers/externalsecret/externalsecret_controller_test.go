07e81f262c059336467b5d51473a0fef586fc841
Jonatas Baldin <jonatas.baldin@gmail.com>
2021-04-12 17:51:51 +0200

    Fix "File is not `gci`-ed" lint error

--- pkg/controllers/externalsecret/externalsecret_controller_test.go
+++ pkg/controllers/externalsecret/externalsecret_controller_test.go
@@ -20,6 +20,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	dto "github.com/prometheus/client_model/go"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -30,8 +31,6 @@ import (
 	"github.com/external-secrets/external-secrets/pkg/provider"
 	"github.com/external-secrets/external-secrets/pkg/provider/fake"
 	"github.com/external-secrets/external-secrets/pkg/provider/schema"
-
-	dto "github.com/prometheus/client_model/go"
 )
 
 var (
