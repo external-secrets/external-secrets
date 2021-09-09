8dd4a19778bc138754d70b5295bde44afb7ce626
Kian <awsometigger@gmail.com>
2021-09-09 23:57:43 +0100

    Fixing assignment issues

--- e2e/run.sh
+++ e2e/run.sh
@@ -61,9 +61,9 @@ kubectl run --rm \
   --env="GITLAB_TOKEN=${GITLAB_TOKEN:-}" \
   --env="GITLAB_PROJECT_ID=${GITLAB_PROJECT_ID:-}" \
   --env="ORACLE_USER_OCID=${ORACLE_USER_OCID:-}" \
-  --env="ORACLE_TENANCY_OCID${ORACLE_TENANCY_OCID:-}" \
-  --env="ORACLE_REGION${ORACLE_REGION:-}" \
-  --env="ORACLE_FINGERPRINT${ORACLE_FINGERPRINT:-}" \
-  --env="ORACLE_KEY${ORACLE_KEY:-}" \
+  --env="ORACLE_TENANCY_OCID=${ORACLE_TENANCY_OCID:-}" \
+  --env="ORACLE_REGION=${ORACLE_REGION:-}" \
+  --env="ORACLE_FINGERPRINT=${ORACLE_FINGERPRINT:-}" \
+  --env="ORACLE_KEY=${ORACLE_KEY:-}" \
   --overrides='{ "apiVersion": "v1", "spec":{"serviceAccountName": "external-secrets-e2e"}}' \
   e2e --image=local/external-secrets-e2e:test
