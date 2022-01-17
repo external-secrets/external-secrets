85c1bcf72115d72fd384e5ff10e30280d2a26894
Sebastian Gomez <sebastiangomezcorrea@gmail.com>
2022-01-17 16:38:47 -0500

    Implement GetAllSecrets in WebHook

--- pkg/provider/webhook/webhook.go
+++ pkg/provider/webhook/webhook.go
@@ -373,6 +373,13 @@ func (w *WebHook) getCertFromConfigMap(provider *esv1alpha1.WebhookProvider) ([]
 	return []byte(val), nil
 }
 
+// Implements store.Client.GetAllSecrets Interface.
+// New version of GetAllSecrets.
+func (sm *WebHook) GetAllSecrets(ctx context.Context, ref esv1alpha1.ExternalSecretDataRemoteRef) (map[string][]byte, error) {
+	// TO be implemented
+	return map[string][]byte{}, nil
+}
+
 func (w *WebHook) Close(ctx context.Context) error {
 	return nil
 }
