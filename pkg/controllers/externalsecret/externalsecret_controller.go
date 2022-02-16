899cf72f2233fc747904b2354bedef1f5230219f
Moritz Johner <beller.moritz@googlemail.com>
2022-02-16 14:37:42 +0100

    feat: avoid caching secrets/configmaps
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- pkg/controllers/externalsecret/externalsecret_controller.go
+++ pkg/controllers/externalsecret/externalsecret_controller.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -423,6 +424,6 @@ func (r *Reconciler) SetupWithManager(mgr ctrl.Manager, opts controller.Options)
 	return ctrl.NewControllerManagedBy(mgr).
 		WithOptions(opts).
 		For(&esv1alpha1.ExternalSecret{}).
-		Owns(&v1.Secret{}).
+		Owns(&v1.Secret{}, builder.OnlyMetadata).
 		Complete(r)
 }
