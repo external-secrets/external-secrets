ac1c696fdb391303410dc50530f31df188c41a0e
Docs <docs@external-secrets.io>
2022-04-11 19:38:03 +0200

    fix: allow control plane ingress to webhook

--- terraform/aws/modules/cluster/main.tf
+++ terraform/aws/modules/cluster/main.tf
@@ -112,6 +112,24 @@ resource "aws_security_group" "additional" {
     ]
   }
 
+  # allow control-plane to access webhook
+  ingress {
+    from_port        = 9443
+    to_port          = 9443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   # 443, 53, 123 is already allowed
   egress {
     from_port        = 80
