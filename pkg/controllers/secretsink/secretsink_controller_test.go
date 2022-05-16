3ec5cb6390bfc6fab23796e0f0b238f89714f722
Tom Godkin <tom.godkin@engineerbetter.com>
2022-05-16 14:55:02 +0100

    Happy path test reconciler.Reconcile
    Signed-off-by: Tom Godkin <tom.godkin@engineerbetter.com>
    
    Co-authored-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
    Co-authored-by: Marcin Kubica <marcin.kubica@engineerbetter.com>
    Co-authored-by: Dom Meddick <dom.meddick@engineerbetter.com>
    Co-authored-by: Will Young <will.young@engineerbetter.com>

--- pkg/controllers/secretsink/secretsink_controller_test.go
+++ pkg/controllers/secretsink/secretsink_controller_test.go
@@ -15,13 +15,40 @@ limitations under the License.
 package secretsink
 
 import (
+	"github.com/external-secrets/external-secrets/pkg/controllers/secretsink/internal"
+	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-var _ = Describe("", func() {
-	It("does nothing", func() {
-		Expect(true).To(BeTrue())
+var _ = Describe("#Reconcile", func() {
+	var (
+		client       *internal.FakeClient
+		statusWriter *internal.FakeStatusWriter
+		reconciler   *Reconciler
+	)
+
+	BeforeEach(func() {
+		client = new(internal.FakeClient)
+		statusWriter = new(internal.FakeStatusWriter)
+		client.StatusReturns(statusWriter)
+		reconciler = &Reconciler{client, logr.Discard(), nil, nil, 0, ""}
 	})
 
+	It("succeeds", func() {
+		namspacedName := types.NamespacedName{Namespace: "foo", Name: "Bar"}
+		_, err := reconciler.Reconcile(nil, ctrl.Request{NamespacedName: namspacedName})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(client.GetCallCount()).To(Equal(1))
+		Expect(client.StatusCallCount()).To(Equal(1))
+
+		_, gotNamespacedName, _ := client.GetArgsForCall(0)
+		Expect(gotNamespacedName).To(Equal(namspacedName))
+
+		Expect(statusWriter.PatchCallCount()).To(Equal(1))
+		_, _, patch, _ := statusWriter.PatchArgsForCall(0)
+		Expect(patch.Type()).To(Equal(types.MergePatchType))
+	})
 })
