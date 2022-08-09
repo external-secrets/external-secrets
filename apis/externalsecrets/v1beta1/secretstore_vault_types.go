da1679fe68105f91e24c19233d58a62e92b16f02
Moritz Johner <beller.moritz@googlemail.com>
2022-08-09 23:16:51 +0200

    fix: deprecate expirationSeconds
    expirationSeconds is not needed because we generate a
    service account token on the fly for a single use.
    There will be no replacement for this.
    
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- apis/externalsecrets/v1beta1/secretstore_vault_types.go
+++ apis/externalsecrets/v1beta1/secretstore_vault_types.go
@@ -194,6 +194,7 @@ type VaultKubernetesServiceAccountTokenAuth struct {
 	// Optional expiration time in seconds that will be used to request a temporary
 	// Kubernetes service account token for the service account referenced by
 	// `serviceAccountRef`.
+	// Deprecated: this will be removed in the future.
 	// Defaults to 10 minutes.
 	// +optional
 	ExpirationSeconds *int64 `json:"expirationSeconds,omitempty"`
