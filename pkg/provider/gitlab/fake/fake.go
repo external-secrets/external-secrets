6c7e5cecce5c6a532a21f7fec00123ee27a4597d
Dominik Zeiger <domizei385@users.noreply.github.com>
2023-01-04 17:58:55 +0100

    :bug: gitlab: Fallback to wildcard variables and use pagination (bugfix) (#1838)
    * gitlab: fallback to wildcard variables when using "GetAllSecrets"
    
    Signed-off-by: Dominik Zeiger <dominik@zeiger.biz>

--- pkg/provider/gitlab/fake/fake.go
+++ pkg/provider/gitlab/fake/fake.go
@@ -14,8 +14,31 @@ limitations under the License.
 package fake
 
 import (
+	"net/http"
+
 	gitlab "github.com/xanzy/go-gitlab"
 )
+
+type APIResponse[O any] struct {
+	Output   O
+	Response *gitlab.Response
+	Error    error
+}
+
+type GitVariable interface {
+	gitlab.ProjectVariable |
+		gitlab.GroupVariable
+}
+
+type extractKey[V GitVariable] func(gv V) string
+
+func keyFromProjectVariable(pv gitlab.ProjectVariable) string {
+	return pv.Key
+}
+
+func keyFromGroupVariable(gv gitlab.GroupVariable) string {
+	return gv.Key
+}
 
 type GitlabMockProjectsClient struct {
 	listProjectsGroups func(pid interface{}, opt *gitlab.ListProjectGroupOptions, options ...gitlab.RequestOptionFunc) ([]*gitlab.ProjectGroup, *gitlab.Response, error)
@@ -46,15 +69,63 @@ func (mc *GitlabMockProjectVariablesClient) ListVariables(pid interface{}, opt *
 	return mc.listVariables(pid)
 }
 
-func (mc *GitlabMockProjectVariablesClient) WithValue(output *gitlab.ProjectVariable, response *gitlab.Response, err error) {
-	if mc != nil {
-		mc.getVariable = func(pid interface{}, key string, options ...gitlab.RequestOptionFunc) (*gitlab.ProjectVariable, *gitlab.Response, error) {
-			return output, response, err
-		}
+func (mc *GitlabMockProjectVariablesClient) WithValue(response APIResponse[[]*gitlab.ProjectVariable]) {
+	mc.WithValues([]APIResponse[[]*gitlab.ProjectVariable]{response})
+}
 
-		mc.listVariables = func(pid interface{}, options ...gitlab.RequestOptionFunc) ([]*gitlab.ProjectVariable, *gitlab.Response, error) {
-			return []*gitlab.ProjectVariable{output}, response, err
+func (mc *GitlabMockProjectVariablesClient) WithValues(responses []APIResponse[[]*gitlab.ProjectVariable]) {
+	if mc != nil {
+		mc.getVariable = mockGetVariable(keyFromProjectVariable, responses)
+		mc.listVariables = mockListVariable(responses)
+	}
+}
+
+func mockGetVariable[V GitVariable](keyExtractor extractKey[V], responses []APIResponse[[]*V]) func(interface{}, string, ...gitlab.RequestOptionFunc) (*V, *gitlab.Response, error) {
+	getCount := -1
+	return func(pid interface{}, key string, options ...gitlab.RequestOptionFunc) (*V, *gitlab.Response, error) {
+		getCount++
+		if getCount > len(responses)-1 {
+			return nil, make404APIResponse(), nil
 		}
+		var match *V
+		for _, v := range responses[getCount].Output {
+			if keyExtractor(*v) == key {
+				match = v
+			}
+		}
+		if match == nil {
+			return nil, make404APIResponse(), nil
+		}
+		return match, responses[getCount].Response, responses[getCount].Error
+	}
+}
+
+func mockListVariable[V GitVariable](responses []APIResponse[[]*V]) func(interface{}, ...gitlab.RequestOptionFunc) ([]*V, *gitlab.Response, error) {
+	listCount := -1
+	return func(pid interface{}, options ...gitlab.RequestOptionFunc) ([]*V, *gitlab.Response, error) {
+		listCount++
+		if listCount > len(responses)-1 {
+			return nil, makeAPIResponse(listCount, len(responses)), nil
+		}
+		return responses[listCount].Output, responses[listCount].Response, responses[listCount].Error
+	}
+}
+
+func make404APIResponse() *gitlab.Response {
+	return &gitlab.Response{
+		Response: &http.Response{
+			StatusCode: http.StatusNotFound,
+		},
+	}
+}
+
+func makeAPIResponse(page, pages int) *gitlab.Response {
+	return &gitlab.Response{
+		Response: &http.Response{
+			StatusCode: http.StatusOK,
+		},
+		CurrentPage: page,
+		TotalPages:  pages,
 	}
 }
 
@@ -80,5 +151,12 @@ func (mc *GitlabMockGroupVariablesClient) WithValue(output *gitlab.GroupVariable
 		mc.listVariables = func(gid interface{}, options ...gitlab.RequestOptionFunc) ([]*gitlab.GroupVariable, *gitlab.Response, error) {
 			return []*gitlab.GroupVariable{output}, response, err
 		}
+	}
+}
+
+func (mc *GitlabMockGroupVariablesClient) WithValues(responses []APIResponse[[]*gitlab.GroupVariable]) {
+	if mc != nil {
+		mc.getVariable = mockGetVariable(keyFromGroupVariable, responses)
+		mc.listVariables = mockListVariable(responses)
 	}
 }
