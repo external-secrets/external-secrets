c522bb9297f01fec090ba24ee016145b2f2cab69
Hiroshi Muraoka <h.muraoka714@gmail.com>
2023-01-11 22:28:53 +0900

    fix: prevent goroutine leak on workload identity reconciliation
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- pkg/provider/gcp/secretmanager/auth.go
+++ pkg/provider/gcp/secretmanager/auth.go
@@ -36,6 +36,7 @@ func NewTokenSource(ctx context.Context, auth esv1beta1.GCPSMAuth, projectID str
 		useMu.Unlock()
 		return nil, fmt.Errorf("unable to initialize workload identity")
 	}
+	defer wi.Close()
 	ts, err = wi.TokenSource(ctx, auth, isClusterKind, kube, namespace)
 	if ts != nil || err != nil {
 		return ts, err
