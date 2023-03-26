b031ccc1a2c7d1be1630a4ca7b6fe86e806f7ebd
Maxim Rubchinsky <maxim@rubchinsky.com>
2023-03-26 20:18:10 +0300

    fix SanitizeErr
    Signed-off-by: Maxim Rubchinsky <maxim.rubchinsky@wiz.io>

--- pkg/provider/alibaba/utils.go
+++ pkg/provider/alibaba/utils.go
@@ -5,12 +5,19 @@ import (
 	"regexp"
 )
 
-var regexReqID = regexp.MustCompile(`request id: (\S+)`)
+var regexReqIDs = []*regexp.Regexp{
+	regexp.MustCompile(`request id: (\S+)`),
+	regexp.MustCompile(`"RequestId":"(\S+)",`),
+}
 
 // SanitizeErr sanitizes the error string
 // because the requestID must not be included in the error.
 // otherwise the secrets keeps syncing.
 func SanitizeErr(err error) error {
+	msg := ""
+	for _, regex := range regexReqIDs {
+		msg = string(regex.ReplaceAll([]byte(err.Error()), nil))
+	}
 
-	return errors.New(string(regexReqID.ReplaceAll([]byte(err.Error()), nil)))
+	return errors.New(msg)
 }
