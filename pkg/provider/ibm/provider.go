b389570c819a8bf92d49a0a93bf9bad2c861cdfe
Yuri Sa <48062171+yuriolisa@users.noreply.github.com>
2023-05-15 17:22:18 +0200

    Creating constants file (#2291)
    * Creating constants file
    
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>
    
    * Fixing lints
    
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>
    
    ---------
    
    Signed-off-by: Yuri Sa <yurimsa@gmail.com>

--- pkg/provider/ibm/provider.go
+++ pkg/provider/ibm/provider.go
@@ -30,7 +30,8 @@ import (
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	esv1beta1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
-	"github.com/external-secrets/external-secrets/pkg/provider/metrics"
+	"github.com/external-secrets/external-secrets/pkg/constants"
+	"github.com/external-secrets/external-secrets/pkg/metrics"
 	utils "github.com/external-secrets/external-secrets/pkg/utils"
 )
 
@@ -353,7 +354,7 @@ func getSecretData(ibm *providerIBM, secretName *string) (sm.SecretIntf, error) 
 		&sm.GetSecretOptions{
 			ID: secretName,
 		})
-	metrics.ObserveAPICall(metrics.ProviderIBMSM, metrics.CallIBMSMGetSecret, err)
+	metrics.ObserveAPICall(constants.ProviderIBMSM, constants.CallIBMSMGetSecret, err)
 	if err != nil {
 		return nil, err
 	}
