dece1c6df96f4896315ecfaf603bcbe4094bcc0f
Gustavo Carvalho <gusfcarvalho@gmail.com>
2023-08-07 20:36:06 -0300

    Fix lint
    Signed-off-by: Gustavo Carvalho <gusfcarvalho@gmail.com>

--- pkg/controllers/externalsecret/externalsecret_controller.go
+++ pkg/controllers/externalsecret/externalsecret_controller.go
@@ -39,7 +39,6 @@ import (
 	// Metrics.
 	"github.com/external-secrets/external-secrets/pkg/controllers/externalsecret/esmetrics"
 	ctrlmetrics "github.com/external-secrets/external-secrets/pkg/controllers/metrics"
-
 	// Loading registered generators.
 	_ "github.com/external-secrets/external-secrets/pkg/generator/register"
 	// Loading registered providers.
