51532ca8a10f23564c8a68075a8ca191525497bb
Moritz Johner <moolen@users.noreply.github.com>
2023-10-29 21:51:39 +0100

    feat: add AKS e2e managed (#2811)
    Migrate azure e2e tests to use the new TFC_* secrets which are
    provisioned through external-secrets/infrastructure.
    Also enable the use of `/ok-to-test-managed provider=azure` command
    to run e2e managed tests that verify integration with AKS and
    Azure Workload Identity (AZWI).
    
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -8,14 +8,28 @@ terraform {
 
 provider "azurerm" {
   features {}
+  # set this to false when running locally
+  use_oidc = true 
+}
+
+data "azurerm_kubernetes_cluster" "default" {
+  depends_on          = [module.test_aks] # refresh cluster state before reading
+  name                = var.cluster_name
+  resource_group_name = var.resource_group_name
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.azurerm_kubernetes_cluster.default.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
   }
 }
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
 
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.default.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+}
--- terraform/azure/resource-group/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "current" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
