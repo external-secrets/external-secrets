648b582c003fbbd50bd6d4d8a1fd035fd030e496
Carlos Vicent Domenech <carlosvicentdomenech@gmail.com>
2023-11-01 15:46:14 +0100

    feat(github): to update schema and add v1alpha1 version

--- apis/externalsecrets/v1alpha1/secretstore_github_types.go
+++ apis/externalsecrets/v1alpha1/secretstore_github_types.go
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta1
+package v1alpha1
 
 import (
 	esmeta "github.com/external-secrets/external-secrets/apis/meta/v1"
