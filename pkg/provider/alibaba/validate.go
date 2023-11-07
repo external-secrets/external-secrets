a166919838ea45a5ae16afac08258a2b19eb537e
laisramos123@gmail.com <laisramos123@gmail.com>
2023-11-06 22:25:56 -0300

    Fixing import errors
    Signed-off-by: laisramos123@gmail.com <laisramos123@gmail.com>

--- pkg/provider/alibaba/validate.go
+++ pkg/provider/alibaba/validate.go
@@ -18,27 +18,24 @@ import (
 	//"encoding/json"
 	"fmt"
 
-	 openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-	 kmssdk "github.com/alibabacloud-go/kms-20160120/v3/client"
-	 util "github.com/alibabacloud-go/tea-utils/v2/service"
-	 credential "github.com/aliyun/credentials-go/credentials"
-	 	"github.com/avast/retry-go/v4"
-		"github.com/tidwall/gjson"
-	corev1 "k8s.io/api/core/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/types"
-	 kclient "sigs.k8s.io/controller-runtime/pkg/client"
+	 //openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	 //kmssdk "github.com/alibabacloud-go/kms-20160120/v3/client"
+	// util "github.com/alibabacloud-go/tea-utils/v2/service"
+	 //credential "github.com/aliyun/credentials-go/credentials"
+	 //	"github.com/avast/retry-go/v4"
+		//"github.com/tidwall/gjson"
+	//corev1 "k8s.io/api/core/v1"
+	//apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	//"k8s.io/apimachinery/pkg/types"
+	// kclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	esv1beta1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	"github.com/external-secrets/external-secrets/pkg/utils"
 )
 // type KeyManagementService struct {
-// 	Client SMInterfacetype KeyManagementService struct {
 // 	Client SMInterface
 // 	Config *openapi.Config
-// // }
-// 	Config *openapi.Config
-// // }
+// }
 // type SMInterface interface {
 // 	GetSecretValue(ctx context.Context, request *kmssdk.GetSecretValueRequest) (*kmssdk.GetSecretValueResponseBody, error)
 // 	Endpoint() string
@@ -47,7 +44,7 @@ import (
 func (kms *KeyManagementService) ValidateStore(store esv1beta1.GenericStore) error {
 	
 		storeSpec := store.GetSpec()
-		  if storeSpec == nil || storeSpec.Provider == nil || storeSpec.Provider.Kubernetes == nil {
+		  if storeSpec == nil || storeSpec.Provider == nil || storeSpec.Provider.Alibaba == nil {
 			return fmt.Errorf("no store type or wrong store type")
 		  }
 		
@@ -59,13 +56,6 @@ func (kms *KeyManagementService) ValidateStore(store esv1beta1.GenericStore) err
 		  return fmt.Errorf("missing alibaba region")
 		 }
 		 
-			
-		 if alibabaSpec.Auth.rrsa != nil {
-	    	if alibabaSpec.Auth.rrsa.ServiceAccountRef != nil {
-			   if err := utils.ValidateReferentServiceAccountSelector(store, *alibabaSpec.Auth.rrsa.ServiceAccountRef); err != nil {
-			   return fmt.Errorf(errInvalidKubeSA, err)
-		    }
-		  }
 		 
 		  accessKeyID := alibabaSpec.Auth.SecretRef.AccessKeyID
 		  err := utils.ValidateSecretSelector(store, accessKeyID)
@@ -73,23 +63,16 @@ func (kms *KeyManagementService) ValidateStore(store esv1beta1.GenericStore) err
 			   return err
 		  }
 	   
-		 if alibabaSpec.Auth.rrsa.oidcProviderArn == "" {
-		  return fmt.Errorf("missing alibaba oidc-provider-arn")
-		  }
-		 if alibabaSpec.Auth.rrsa.oidcTokenFilePath == "" {
-		  return fmt.Errorf("missing alibaba oidc-token-file-path")
-		  }
-		  if alibabaSpec.Auth.rrsa.roleArn == "" {
-		   return fmt.Errorf("missing alibaba role-arn")
-		  }
-		  if alibabaSpec.Auth.rrsa.sessionName == "" {
-		   return fmt.Errorf("missing alibaba session-name")
-		  }  
-		 
-		  return nil
-		 }
+		
 		return kms.validateStoreAuth(store)
 }
-	   
+
+
+
+func init() {
+	esv1beta1.Register(&KeyManagementService{}, &esv1beta1.SecretStoreProvider{
+		Alibaba: &esv1beta1.AlibabaProvider{},
+	})
+}
 	
    
