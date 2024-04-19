ec4d6625fcfbb203b6e7a773d020cb9fdb4065e3
Bill Hamilton <bill.hamilton@delinea.com>
2024-04-19 08:00:11 -0700

    commiting initial e2e testing code
    Signed-off-by: Bill Hamilton <bill.hamilton@delinea.com>

--- e2e/go.mod
+++ e2e/go.mod
@@ -87,6 +87,7 @@ require (
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
+	github.com/DelineaXPM/tss-sdk-go/v2 v2.0.1 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
--- e2e/suites/provider/cases/secretserver/provider.go
+++ e2e/suites/provider/cases/secretserver/provider.go
@@ -1,7 +1,9 @@
-package delinea
+package secretserver
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 
 	"github.com/DelineaXPM/tss-sdk-go/v2/server"
 /*	"github.com/DelineaXPM/dsv-sdk-go/v2/vault"*/
@@ -19,8 +21,8 @@ func (p *secretStoreProvider) init(cfg *config) {
 	p.cfg = cfg
 
 	secretserverClient, err := server.New(server.Configuration{
-		Credentials: vault.ClientCredential{
-			Username:     cfg.username,
+		Credentials: server.UserCredential{
+			Username: cfg.username,
 			Password: cfg.password,
 		},
 		ServerURL:      cfg.serverURL,
@@ -34,13 +36,33 @@ func (p *secretStoreProvider) CreateSecret(key string, val framework.SecretEntry
 	var data map[string]interface{}
 	err := json.Unmarshal([]byte(val.Value), &data)
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	_, err = p.api.CreateSecret(key, &vault.SecretCreateRequest{
-		Data: data,
+
+	fields := make([]server.SecretField, 3)
+		fields[0].FieldID = 108 // machine
+		fields[0].ItemValue = "Secret Server TEST MACHINE"
+		fields[1].FieldID = 111 // username
+		fields[1].ItemValue = "secretserver_username"
+		fields[2].FieldID = 110 // password
+		fields[2].ItemValue = "secretserver_password"
+
+	_, err = p.api.CreateSecret(server.Secret{
+		SecretTemplateID: 6007,
+		SiteID: 1,
+		FolderID: 73,
+		Name: key,
+		Fields: fields,
 	})
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 }
 
 func (p *secretStoreProvider) DeleteSecret(key string) {
-	err := p.api.DeleteSecret(key)
+	fmt.Println("DELETE SECRET KEY = ", key)
+	id, _ := strconv.Atoi(key)
+/*
+	if err != nil {
+		return nil, errors.New("incorrect string to integer conversion")
+	}
+*/
+	err := p.api.DeleteSecret(id)
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 }
