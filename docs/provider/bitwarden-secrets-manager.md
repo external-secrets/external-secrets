645797addbde0a984594ef6883de4b97eef53646
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-06-20 15:38:16 +0200

    add document skeleton and fix helm dependency init
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>

---
+++ docs/provider/bitwarden-secrets-manager.md
@@ -0,0 +1,19 @@
+## Bitwarden Secrets Manager Provider
+
+This section describes how to set up the Bitwarden Secrets Manager provider for External Secrets Operator (ESO).
+
+### Prerequisites
+
+#### Bitwarden SDK server
+
+##### Certificate
+
+### External secret store
+
+#### Step 1: Define an external secret store
+
+#### Step 2: Create the external secrets store
+
+#### Find by Name or Find by UUID
+
+### Push Secret
--- pkg/provider/bitwarden/client_test.go
+++ pkg/provider/bitwarden/client_test.go
@@ -1,3 +1,17 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package bitwarden
 
 import (
@@ -5,9 +19,10 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 )
 
 func TestProvider_DeleteSecret(t *testing.T) {
