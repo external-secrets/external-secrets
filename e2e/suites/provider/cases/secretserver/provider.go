830c9788d8e7aef3c950e5fb47fc48502465dd1e
Bill Hamilton <bill.hamilton@pacificcode.com>
2024-07-08 07:03:57 -0700

    Update provider.go
    changed params in provider.go for secretserver e2e test
    
    Signed-off-by: Bill Hamilton <bill.hamilton@pacificcode.com>

--- e2e/suites/provider/cases/secretserver/provider.go
+++ e2e/suites/provider/cases/secretserver/provider.go
@@ -38,13 +38,13 @@ func (p *secretStoreProvider) CreateSecret(key string, val framework.SecretEntry
 	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	fields := make([]server.SecretField, 1)
-	fields[0].FieldID = 439 // Data
+	fields[0].FieldID = 195 // Data
 	fields[0].ItemValue = val.Value
 
 	s, err := p.api.CreateSecret(server.Secret{
-		SecretTemplateID: 6098, // custom template
+		SecretTemplateID: 6027, // custom template
 		SiteID: 1,
-		FolderID: 73,
+		FolderID: 10,
 		Name: key,
 		Fields: fields,
 	})
