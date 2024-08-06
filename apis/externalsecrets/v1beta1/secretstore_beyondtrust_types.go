91ef4134784d5f1bbd0b3682c0766b2636212142
Felipe Hernandez <fhernandez@beyondtrust.com>
2024-08-06 13:41:53 -0500

    fix: apply camelCase to yaml attributes
    Signed-off-by: Felipe Hernandez <fhernandez@beyondtrust.com>

--- apis/externalsecrets/v1beta1/secretstore_beyondtrust_types.go
+++ apis/externalsecrets/v1beta1/secretstore_beyondtrust_types.go
@@ -30,27 +30,27 @@ type BeyondTrustProviderSecretRef struct {
 // Configures a store to sync secrets using BeyondTrust Password Safe.
 type BeyondtrustAuth struct {
 	// +required - API OAuth Client ID.
-	ClientID *BeyondTrustProviderSecretRef `json:"clientid"`
+	ClientID *BeyondTrustProviderSecretRef `json:"clientId"`
 	// +required - API OAuth Client Secret.
-	ClientSecret *BeyondTrustProviderSecretRef `json:"clientsecret"`
+	ClientSecret *BeyondTrustProviderSecretRef `json:"clientSecret"`
 	// Content of the certificate (cert.pem) for use when authenticating with an OAuth client Id using a Client Certificate.
 	Certificate *BeyondTrustProviderSecretRef `json:"certificate,omitempty"`
 	// Certificate private key (key.pem). For use when authenticating with an OAuth client Id
-	CertificateKey *BeyondTrustProviderSecretRef `json:"certificatekey,omitempty"`
+	CertificateKey *BeyondTrustProviderSecretRef `json:"certificateKey,omitempty"`
 }
 
 // Configures a store to sync secrets using BeyondTrust Password Safe.
 type BeyondtrustServer struct {
 	// +required - BeyondTrust Password Safe API URL. https://example.com:443/beyondtrust/api/public/V3.
-	APIURL string `json:"apiurl"`
+	APIURL string `json:"apiUrl"`
 	// The secret retrieval type. SECRET = Secrets Safe (credential, text, file). MANAGED_ACCOUNT = Password Safe account associated with a system.
-	RetrievalType string `json:"retrievaltype,omitempty"`
+	RetrievalType string `json:"retrievalType,omitempty"`
 	// A character that separates the folder names.
 	Separator string `json:"separator,omitempty"`
 	// +required - Indicates whether to verify the certificate authority on the Secrets Safe instance. Warning - false is insecure, instructs the BT provider not to verify the certificate authority.
-	VerifyCA bool `json:"verifyca"`
+	VerifyCA bool `json:"verifyCA"`
 	// Timeout specifies a time limit for requests made by this Client. The timeout includes connection time, any redirects, and reading the response body. Defaults to 45 seconds.
-	ClientTimeOutSeconds int `json:"clienttimeoutseconds,omitempty"`
+	ClientTimeOutSeconds int `json:"clientTimeOutSeconds,omitempty"`
 }
 
 type BeyondtrustProvider struct {
