360a9c74f18edf35b85ec0922f935004483a7f8f
Gustavo Carvalho <gustavo.carvalho@productmadness.com>
2024-09-14 10:16:15 -0300

    fix: revert license change
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@productmadness.com>

--- apis/generators/v1alpha1/register.go
+++ apis/generators/v1alpha1/register.go
@@ -1,4 +1,16 @@
-//Copyright External Secrets Inc. All Rights Reserved
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package v1alpha1
 
@@ -88,17 +100,8 @@ var (
 	GithubAccessTokenGroupVersionKind = SchemeGroupVersion.WithKind(GithubAccessTokenKind)
 )
 
-// AWSIAMKeys type metadata.
-var (
-	AWSIAMKeysKind             = reflect.TypeOf(AWSIAMKey{}).Name()
-	AWSIAMKeysGroupKind        = schema.GroupKind{Group: Group, Kind: AWSIAMKeysKind}.String()
-	AWSIAMKeysKindAPIVersion   = AWSIAMKeysKind + "." + SchemeGroupVersion.String()
-	AWSIAMKeysGroupVersionKind = SchemeGroupVersion.WithKind(AWSIAMKeysKind)
-)
-
 func init() {
-	SchemeBuilder.Register(&ECRAuthorizationToken{}, &ECRAuthorizationTokenList{})
-	SchemeBuilder.Register(&AWSIAMKey{}, &AWSIAMKeysList{})
+	SchemeBuilder.Register(&ECRAuthorizationToken{}, &ECRAuthorizationToken{})
 	SchemeBuilder.Register(&GCRAccessToken{}, &GCRAccessTokenList{})
 	SchemeBuilder.Register(&GithubAccessToken{}, &GithubAccessTokenList{})
 	SchemeBuilder.Register(&ACRAccessToken{}, &ACRAccessTokenList{})
