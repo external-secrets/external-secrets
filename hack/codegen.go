b658d2698971673f7d52f8ace4e6d6b6f1344cc4
Amine Benseddik <amine@pixelfactory.io>
2024-10-22 16:22:12 +0200

    chore: make reviewable
    Signed-off-by: Amine Benseddik <amine@pixelfactory.io>

--- hack/codegen.go
+++ hack/codegen.go
@@ -1,6 +1,19 @@
+// /*
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
 package hack
 
-// Keep a reference to code-generator so it's not removed by go mod tidy
+// Keep a reference to code-generator so it's not removed by go mod tidy.
 import (
 	_ "k8s.io/code-generator"
 )
