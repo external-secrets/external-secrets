2cc28c5c764e5234c7424abbebe86bec7bf4190c
SYSHIL <ilhan.syed@gmail.com>
2025-05-11 17:08:43 +0530

    more lint fixes
    Signed-off-by: SYSHIL <ilhan.syed@gmail.com>

--- pkg/provider/aws/parameterstore/resolver.go
+++ pkg/provider/aws/parameterstore/resolver.go
@@ -1,16 +1,16 @@
-// /*
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// */
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 package parameterstore
 
@@ -24,15 +24,13 @@ import (
 	smithyendpoints "github.com/aws/smithy-go/endpoints"
 )
 
-const (
-	SSMEndpointEnv = "AWS_SSM_ENDPOINT"
-)
+// SSMEndpointEnv is the environment variable to use for Parameter Store endpoint.
+const SSMEndpointEnv = "AWS_SSM_ENDPOINT"
 
+// customEndpointResolver is a custom resolver for AWS Parameter Store endpoint.
 type customEndpointResolver struct{}
 
-// ResolveEndpoint returns a ResolverFunc with
-// customizable endpoints.
-
+// ResolveEndpoint resolves the endpoint for the Parameter Store service.
 func (c customEndpointResolver) ResolveEndpoint(ctx context.Context, params ssm.EndpointParameters) (smithyendpoints.Endpoint, error) {
 	endpoint := smithyendpoints.Endpoint{}
 	if v := os.Getenv(SSMEndpointEnv); v != "" {
