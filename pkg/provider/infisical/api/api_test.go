ba2f4c7c9ed32a7df8082c431d2884f16d156e4f
Daniel Hougaard <daniel@infisical.com>
2025-05-30 00:12:09 +0400

    Update api_test.go
    Signed-off-by: Daniel Hougaard <daniel@infisical.com>

--- pkg/provider/infisical/api/api_test.go
+++ pkg/provider/infisical/api/api_test.go
@@ -16,13 +16,15 @@ package api
 
 import (
 	"errors"
-	"reflect"
 	"testing"
 
+	infisical "github.com/infisical/go-sdk"
 	"github.com/stretchr/testify/assert"
 
 	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
 )
+
+var errNoAccessToken = errors.New("unexpected error: no access token available to revoke")
 
 const (
 	fakeClientID        = "client-id"
@@ -32,94 +34,6 @@ const (
 	fakeEnvironmentSlug = "dev"
 )
 
-func TestAPIClientDo(t *testing.T) {
-	apiURL := "foo"
-	httpMethod := "bar"
-
-	testCases := []struct {
-		Name             string
-		MockStatusCode   int
-		MockResponse     any
-		ExpectedResponse any
-		ExpectedError    error
-	}{
-		{
-			Name:           "Success",
-			MockStatusCode: 200,
-			MockResponse: MachineIdentityDetailsResponse{
-				AccessToken: "foobar",
-			},
-			ExpectedResponse: MachineIdentityDetailsResponse{
-				AccessToken: "foobar",
-			},
-			ExpectedError: nil,
-		},
-		{
-			Name:           "Error when response cannot be unmarshalled",
-			MockStatusCode: 500,
-			MockResponse:   []byte("not-json"),
-			ExpectedError:  errors.New("API error (500), could not unmarshal error response: json: cannot unmarshal string into Go value of type api.InfisicalAPIErrorResponse"),
-		},
-		{
-			Name:           "Error when non-Infisical error response received",
-			MockStatusCode: 500,
-			MockResponse:   map[string]string{"foo": "bar"},
-			ExpectedError:  errors.New("API error (500): {\"foo\":\"bar\"}"),
-		},
-		{
-			Name:           "Do: Error when non-200 response received",
-			MockStatusCode: 401,
-			MockResponse: InfisicalAPIErrorResponse{
-				StatusCode: 401,
-				Error:      "Unauthorized",
-			},
-			ExpectedError: &InfisicalAPIError{StatusCode: 401, Err: "Unauthorized", Message: ""},
-		},
-		{
-			Name:           "Error when arbitrary details are returned",
-			MockStatusCode: 401,
-			MockResponse: InfisicalAPIErrorResponse{
-				StatusCode: 401,
-				Error:      "Unauthorized",
-				Details:    map[string]string{"foo": "details"},
-			},
-			ExpectedError: &InfisicalAPIError{StatusCode: 401, Err: "Unauthorized", Message: "", Details: map[string]string{"foo": "details"}},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.Name, func(t *testing.T) {
-			apiClient, closeFunc := NewMockClient(tc.MockStatusCode, tc.MockResponse)
-			defer closeFunc()
-
-			// Automatically pluck out the expected response type using reflection to create a new empty value for unmarshalling.
-			var actualResponse any
-			if tc.ExpectedResponse != nil {
-				actualResponse = reflect.New(reflect.TypeOf(tc.ExpectedResponse)).Interface()
-			}
-
-			err := apiClient.do(apiURL, httpMethod, nil, nil, actualResponse)
-			if tc.ExpectedError != nil {
-				assert.Error(t, err)
-				assert.Equal(t, tc.ExpectedError.Error(), err.Error())
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tc.ExpectedResponse, reflect.ValueOf(actualResponse).Elem().Interface())
-			}
-		})
-	}
-}
-
-// TestAPIClientDoInvalidResponse tests the case where the response is a 200 but does not unmarshal
-// correctly.
-func TestAPIClientDoInvalidResponse(t *testing.T) {
-	apiClient, closeFunc := NewMockClient(200, []byte("not-json"))
-	defer closeFunc()
-
-	err := apiClient.do("foo", "bar", nil, nil, nil)
-	assert.ErrorIs(t, err, errJSONUnmarshal)
-}
-
 func TestSetTokenViaMachineIdentity(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		apiClient, closeFunc := NewMockClient(200, MachineIdentityDetailsResponse{
@@ -127,9 +41,9 @@ func TestSetTokenViaMachineIdentity(t *testing.T) {
 		})
 		defer closeFunc()
 
-		err := apiClient.SetTokenViaMachineIdentity(fakeClientID, fakeClientSecret)
+		_, err := apiClient.Auth().UniversalAuthLogin(fakeClientID, fakeClientSecret)
 		assert.NoError(t, err)
-		assert.Equal(t, apiClient.token, "foobar")
+		assert.Equal(t, apiClient.Auth().GetAccessToken(), "foobar")
 	})
 
 	t.Run("SetTokenViaMachineIdentity: Error when non-200 response received", func(t *testing.T) {
@@ -139,22 +53,12 @@ func TestSetTokenViaMachineIdentity(t *testing.T) {
 		})
 		defer closeFunc()
 
-		err := apiClient.SetTokenViaMachineIdentity(fakeClientID, fakeClientSecret)
+		_, err := apiClient.Auth().UniversalAuthLogin(fakeClientID, fakeClientSecret)
 		assert.Error(t, err)
 		var apiErr *InfisicalAPIError
 		assert.True(t, errors.As(err, &apiErr))
 		assert.Equal(t, 401, apiErr.StatusCode)
 		assert.Equal(t, "Unauthorized", apiErr.Err)
-	})
-
-	t.Run("Error when token already set", func(t *testing.T) {
-		apiClient, closeFunc := NewMockClient(401, nil)
-		defer closeFunc()
-
-		apiClient.token = fakeToken
-
-		err := apiClient.SetTokenViaMachineIdentity(fakeClientID, fakeClientSecret)
-		assert.ErrorIs(t, err, errAccessTokenAlreadyRetrieved)
 	})
 }
 
@@ -165,12 +69,13 @@ func TestRevokeAccessToken(t *testing.T) {
 		})
 		defer closeFunc()
 
-		apiClient.token = fakeToken
+		apiClient.Auth().SetAccessToken(fakeToken)
 
-		err := apiClient.RevokeAccessToken()
+		err := apiClient.Auth().RevokeAccessToken()
+
 		assert.NoError(t, err)
 		// Verify that the access token was unset.
-		assert.Equal(t, apiClient.token, "")
+		assert.Equal(t, apiClient.Auth().GetAccessToken(), "")
 	})
 
 	t.Run("RevokeAccessToken: Error when non-200 response received", func(t *testing.T) {
@@ -180,9 +85,9 @@ func TestRevokeAccessToken(t *testing.T) {
 		})
 		defer closeFunc()
 
-		apiClient.token = fakeToken
+		apiClient.Auth().SetAccessToken(fakeToken)
 
-		err := apiClient.RevokeAccessToken()
+		err := apiClient.Auth().RevokeAccessToken()
 		assert.Error(t, err)
 		var apiErr *InfisicalAPIError
 		assert.True(t, errors.As(err, &apiErr))
@@ -194,7 +99,7 @@ func TestRevokeAccessToken(t *testing.T) {
 		apiClient, closeFunc := NewMockClient(401, nil)
 		defer closeFunc()
 
-		err := apiClient.RevokeAccessToken()
+		err := apiClient.Auth().RevokeAccessToken()
 		assert.ErrorIs(t, err, errNoAccessToken)
 	})
 }
@@ -208,11 +113,11 @@ func TestGetSecretsV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		secrets, err := apiClient.GetSecretsV3(GetSecretsV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			Recursive:       true,
+		secrets, err := apiClient.Secrets().List(infisical.ListSecretsOptions{
+			ProjectSlug: fakeProjectSlug,
+			Environment: fakeEnvironmentSlug,
+			SecretPath:  "/",
+			Recursive:   true,
 		})
 		assert.NoError(t, err)
 		assert.Equal(t, secrets, map[string]string{"foo": "bar"})
@@ -226,11 +131,12 @@ func TestGetSecretsV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		secrets, err := apiClient.GetSecretsV3(GetSecretsV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			Recursive:       true,
+		secrets, err := apiClient.Secrets().List(infisical.ListSecretsOptions{
+			ProjectSlug:    fakeProjectSlug,
+			Environment:    fakeEnvironmentSlug,
+			IncludeImports: true,
+			SecretPath:     "/",
+			Recursive:      true,
 		})
 		assert.NoError(t, err)
 		assert.Equal(t, secrets, map[string]string{"foo": "bar"})
@@ -243,11 +149,11 @@ func TestGetSecretsV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		_, err := apiClient.GetSecretsV3(GetSecretsV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			Recursive:       true,
+		_, err := apiClient.Secrets().List(infisical.ListSecretsOptions{
+			ProjectSlug: fakeProjectSlug,
+			Environment: fakeEnvironmentSlug,
+			SecretPath:  "/",
+			Recursive:   true,
 		})
 		assert.Error(t, err)
 		var apiErr *InfisicalAPIError
@@ -266,11 +172,12 @@ func TestGetSecretByKeyV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		secret, err := apiClient.GetSecretByKeyV3(GetSecretByKeyV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			SecretKey:       "foo",
+		secret, err := apiClient.Secrets().Retrieve(infisical.RetrieveSecretOptions{
+			ProjectSlug:    fakeProjectSlug,
+			Environment:    fakeEnvironmentSlug,
+			SecretPath:     "/",
+			IncludeImports: true,
+			SecretKey:      "foo",
 		})
 		assert.NoError(t, err)
 		assert.Equal(t, "bar", secret)
@@ -283,11 +190,12 @@ func TestGetSecretByKeyV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		_, err := apiClient.GetSecretByKeyV3(GetSecretByKeyV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			SecretKey:       "foo",
+		_, err := apiClient.Secrets().Retrieve(infisical.RetrieveSecretOptions{
+			ProjectSlug:    fakeProjectSlug,
+			Environment:    fakeEnvironmentSlug,
+			SecretPath:     "/",
+			IncludeImports: true,
+			SecretKey:      "foo",
 		})
 		assert.Error(t, err)
 		// Importantly, we return the standard error for no secrets found.
@@ -302,11 +210,12 @@ func TestGetSecretByKeyV3(t *testing.T) {
 		})
 		defer closeFunc()
 
-		_, err := apiClient.GetSecretByKeyV3(GetSecretByKeyV3Request{
-			ProjectSlug:     fakeProjectSlug,
-			EnvironmentSlug: fakeEnvironmentSlug,
-			SecretPath:      "/",
-			SecretKey:       "foo",
+		_, err := apiClient.Secrets().Retrieve(infisical.RetrieveSecretOptions{
+			ProjectSlug:    fakeProjectSlug,
+			Environment:    fakeEnvironmentSlug,
+			SecretPath:     "/",
+			IncludeImports: true,
+			SecretKey:      "foo",
 		})
 		assert.Error(t, err)
 		var apiErr *InfisicalAPIError
