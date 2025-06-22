982f5423faef6b5cd92291d257faa616328134a8
SYSHIL <ilhan.syed@gmail.com>
2025-06-22 15:39:55 +0530

    fix auth
    Signed-off-by: SYSHIL <ilhan.syed@gmail.com>

--- pkg/provider/aws/auth/auth.go
+++ pkg/provider/aws/auth/auth.go
@@ -196,20 +196,23 @@ func NewGeneratorSession(ctx context.Context, auth esv1.AWSAuth, role, region st
 		}
 	}
 
-	config := aws.NewConfig()
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithCredentialsProvider(credsProvider))
+	if err != nil {
+		return nil, err
+	}
 	if credsProvider != nil {
-		config.Credentials = credsProvider
+		cfg.Credentials = credsProvider
 	}
 	if region != "" {
-		config.Region = region
+		cfg.Region = region
 	}
 
 	if role != "" {
-		stsclient := assumeRoler(*config)
-		config.Credentials = stscreds.NewAssumeRoleProvider(stsclient, role)
+		stsclient := assumeRoler(cfg)
+		cfg.Credentials = stscreds.NewAssumeRoleProvider(stsclient, role)
 	}
-	log.Info("using aws config", "region", config.Region, "credentials", config.Credentials)
-	return config, nil
+	log.Info("using aws config", "region", cfg.Region, "credentials", cfg.Credentials)
+	return &cfg, nil
 }
 
 // credsFromSecretRef pulls access-key / secret-access-key from a secretRef to
