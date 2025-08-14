b1c0dbbeecae5564c774e41f489da0568b60ce85
Jonathan Stacks <jonstacks@users.noreply.github.com>
2025-08-14 15:00:40 -0500

    feat: Add ngrok provider
    Signed-off-by: Jonathan Stacks <jonstacks@users.noreply.github.com>

---
+++ pkg/provider/ngrok/fake/fake.go
@@ -0,0 +1,222 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+import (
+	"context"
+	"maps"
+	"math/rand"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/ngrok/ngrok-api-go/v7"
+)
+
+func GenerateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano())) /* #nosec G404 */
+
+	sb := strings.Builder{}
+	sb.Grow(length)
+	for i := 0; i < length; i++ {
+		sb.WriteByte(charset[seededRand.Intn(len(charset))])
+	}
+	return sb.String()
+}
+
+type VaultClient struct {
+	vaults    map[string]*ngrok.Vault
+	createErr error
+	listErr   error
+}
+
+func NewVaultClient(vaults ...*ngrok.Vault) *VaultClient {
+	m := make(map[string]*ngrok.Vault)
+	for _, v := range vaults {
+		m[v.ID] = v
+	}
+	return &VaultClient{
+		vaults: m,
+	}
+}
+
+// WithCreateError sets an error to be returned when Create is called.
+// This is useful for testing error handling in the client.
+func (m *VaultClient) WithCreateError(err error) *VaultClient {
+	m.createErr = err
+	return m
+}
+
+// Create creates a new vault and returns it. If an error is set, it will return that error instead of the vault.
+func (m *VaultClient) Create(_ context.Context, vault *ngrok.VaultCreate) (*ngrok.Vault, error) {
+	newVault := &ngrok.Vault{
+		ID:          "vault_" + GenerateRandomString(20),
+		Name:        vault.Name,
+		Description: vault.Description,
+	}
+	m.vaults[newVault.ID] = newVault
+	return newVault, m.createErr
+}
+
+// WithListError sets an error to be returned when List is called.
+func (m *VaultClient) WithListError(err error) *VaultClient {
+	m.listErr = err
+	return m
+}
+
+// List returns an iterator over the vaults.
+// If an error is set, it will return that error instead of the vaults.
+func (m *VaultClient) List(paging *ngrok.Paging) ngrok.Iter[*ngrok.Vault] {
+	items := slices.Collect(maps.Values(m.vaults))
+	return NewIter(items, m.listErr)
+}
+
+// SecretsClient is a mock implementation of the SecretsClient interface.
+// It allows you to create, update, delete, and list secrets.
+// It can be used to test the client without needing a real ngrok API.
+type SecretsClient struct {
+	secrets   map[string]*ngrok.Secret
+	createErr error
+	updateErr error
+	deleteErr error
+	listErr   error
+}
+
+// NewSecretsClient creates a new SecretsClient with the given secrets.
+// It initializes the secrets map with the provided secrets.
+func NewSecretsClient(secrets ...*ngrok.Secret) *SecretsClient {
+	m := make(map[string]*ngrok.Secret)
+	for _, s := range secrets {
+		m[s.ID] = s
+	}
+	return &SecretsClient{
+		secrets: m,
+	}
+}
+
+// WithCreateError sets an error to be returned when Create is called.
+// This is useful for testing error handling in the client.
+func (m *SecretsClient) WithCreateError(err error) *SecretsClient {
+	m.createErr = err
+	return m
+}
+
+// Create creates a new secret and returns it. If an error is set, it will return that error instead of the secret.
+func (m *SecretsClient) Create(_ context.Context, secret *ngrok.SecretCreate) (*ngrok.Secret, error) {
+	newSecret := &ngrok.Secret{
+		ID: "secret_" + GenerateRandomString(20),
+		Vault: ngrok.Ref{
+			ID:  secret.VaultID,
+			URI: "vaults/" + secret.VaultID,
+		},
+		Name:        secret.Name,
+		Description: secret.Description,
+	}
+	m.secrets[newSecret.ID] = newSecret
+	return newSecret, m.createErr
+}
+
+// WithUpdateError sets an error to be returned when Update is called.
+// This is useful for testing error handling in the client.
+func (m *SecretsClient) WithUpdateError(err error) *SecretsClient {
+	m.updateErr = err
+	return m
+}
+
+// Update updates an existing secret and returns it. If an error is set, it will return that error instead of the secret.
+func (m *SecretsClient) Update(_ context.Context, secret *ngrok.SecretUpdate) (*ngrok.Secret, error) {
+	for i, s := range m.secrets {
+		if s.ID != secret.ID {
+			continue
+		}
+
+		if secret.Description != nil {
+			s.Description = *secret.Description
+		}
+		return m.secrets[i], m.updateErr
+	}
+	return nil, &ngrok.Error{StatusCode: 404, Msg: "Secret not found"}
+}
+
+// WithDeleteError sets an error to be returned when Delete is called.
+// This is useful for testing error handling in the client.
+func (m *SecretsClient) WithDeleteError(err error) *SecretsClient {
+	m.deleteErr = err
+	return m
+}
+
+// Delete deletes a secret by its ID. If an error is set, it will return that error instead of deleting the secret.
+// If the secret does not exist, it returns an error.
+func (m *SecretsClient) Delete(_ context.Context, secretID string) error {
+	_, ok := m.secrets[secretID]
+	if !ok {
+		return &ngrok.Error{StatusCode: 404, Msg: "Secret not found"}
+	}
+	delete(m.secrets, secretID)
+	return m.deleteErr
+}
+
+// WithListError sets an error to be returned when List is called.
+// This is useful for testing error handling in the client.
+func (m *SecretsClient) WithListError(err error) *SecretsClient {
+	m.listErr = err
+	return m
+}
+
+// List returns an iterator over the secrets.
+// If an error is set, it will return that error instead of the secrets.
+func (m *SecretsClient) List(paging *ngrok.Paging) ngrok.Iter[*ngrok.Secret] {
+	items := slices.Collect(maps.Values(m.secrets))
+	return NewIter(items, m.listErr)
+}
+
+// Iter is a mock iterator that implements the ngrok.Iter[T] interface.
+type Iter[T any] struct {
+	items []T
+	err   error
+	n     int
+}
+
+func (m *Iter[T]) Next(_ context.Context) bool {
+	// If there is an error, stop iteration
+	if m.err != nil {
+		return false
+	}
+
+	// Increment the index
+	m.n++
+
+	return m.n < len(m.items) && m.n >= 0
+}
+
+func (m *Iter[T]) Item() T {
+	if m.n >= 0 && m.n < len(m.items) {
+		return m.items[m.n]
+	}
+	return *new(T)
+}
+
+func (m *Iter[T]) Err() error {
+	return m.err
+}
+
+func NewIter[T any](items []T, err error) *Iter[T] {
+	return &Iter[T]{
+		items: items,
+		err:   err,
+		n:     -1,
+	}
+}
---
+++ pkg/provider/ngrok/provider.go
@@ -0,0 +1,140 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impliec.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ngrok
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
+	kubeClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
+	"github.com/external-secrets/external-secrets/pkg/utils/resolvers"
+	"github.com/ngrok/ngrok-api-go/v7"
+	"github.com/ngrok/ngrok-api-go/v7/secrets"
+	"github.com/ngrok/ngrok-api-go/v7/vaults"
+)
+
+var (
+	defaultAPIURL = "https://api.ngrok.com"
+	userAgent     = "external-secrets"
+
+	errInvalidStore     = errors.New("invalid store")
+	errInvalidStoreSpec = errors.New("invalid store spec")
+	errInvalidStoreProv = errors.New("invalid store provider")
+	errInvalidNgrokProv = errors.New("invalid ngrok provider")
+	errInvalidAPIURL    = errors.New("invalid API URL")
+)
+
+type Provider struct{}
+
+// Capabilities returns the provider supported capabilities (ReadOnly, WriteOnly, ReadWrite). Currently,
+// ngrok only supports WriteOnly capabilities.
+func (p *Provider) Capabilities() esv1.SecretStoreCapabilities {
+	return esv1.SecretStoreWriteOnly
+}
+
+// NewClient implements the Client interface.
+func (p *Provider) NewClient(ctx context.Context, store esv1.GenericStore, kubeClient kubeClient.Client, namespace string) (esv1.SecretsClient, error) {
+	cfg, err := getConfig(store)
+	if err != nil {
+		return nil, err
+	}
+
+	if store.GetKind() == esv1.ClusterSecretStoreKind && doesConfigDependOnNamespace(cfg) {
+		return nil, errors.New("when using a ClusterSecretStore, namespaces must be explicitly set")
+	}
+
+	apiKey, err := loadAPIKeySecret(ctx, cfg.APIKey, kubeClient, store.GetKind(), namespace)
+	if err != nil {
+		return nil, err
+	}
+
+	clientConfig := ngrok.NewClientConfig(
+		apiKey,
+		ngrok.WithBaseURL(cfg.APIURL),
+		ngrok.WithUserAgent(userAgent),
+	)
+
+	vaultClient := vaults.NewClient(clientConfig)
+	secretsClient := secrets.NewClient(clientConfig)
+
+	return &client{
+		vaultClient:   vaultClient,
+		secretsClient: secretsClient,
+		vaultName:     cfg.VaultName,
+	}, nil
+}
+
+// ValidateStore validates the store configuration.
+func (p *Provider) ValidateStore(store esv1.GenericStore) (admission.Warnings, error) {
+	_, err := getConfig(store)
+	return nil, err
+}
+
+func loadAPIKeySecret(ctx context.Context, ref *esv1.NgrokProviderSecretRef, kube kubeClient.Client, storeKind, namespace string) (string, error) {
+	return resolvers.SecretKeyRef(
+		ctx,
+		kube,
+		storeKind,
+		namespace,
+		ref.SecretRef,
+	)
+}
+
+func doesConfigDependOnNamespace(cfg *esv1.NgrokProvider) bool {
+	return cfg.APIKey.SecretRef != nil && cfg.APIKey.SecretRef.Namespace == nil
+}
+
+func getConfig(store esv1.GenericStore) (*esv1.NgrokProvider, error) {
+	if store == nil {
+		return nil, errInvalidStore
+	}
+
+	storeSpec := store.GetSpec()
+	if storeSpec == nil {
+		return nil, errInvalidStoreSpec
+	}
+
+	if storeSpec.Provider == nil {
+		return nil, errInvalidStoreProv
+	}
+
+	cfg := storeSpec.Provider.Ngrok
+	if cfg == nil {
+		return nil, errInvalidNgrokProv
+	}
+
+	if cfg.APIURL == "" {
+		cfg.APIURL = defaultAPIURL
+	} else if _, err := url.Parse(cfg.APIURL); err != nil {
+		return nil, fmt.Errorf("%q: %w", cfg.APIURL, errInvalidAPIURL)
+	}
+
+	return cfg, nil
+}
+
+func init() {
+	esv1.Register(
+		&Provider{},
+		&esv1.SecretStoreProvider{
+			Ngrok: &esv1.NgrokProvider{},
+		},
+		esv1.MaintenanceStatusMaintained,
+	)
+}
