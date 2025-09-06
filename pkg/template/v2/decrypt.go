4e70fcde35a0f8362952efbcd3ff647e19456125
Felipe Oliveira dos Santos <felipeoliveira.s.br@gmail.com>
2025-09-06 16:07:47 -0400

    test: building an rsa-based sensitive data decryption feature with advanced templating v2
    Signed-off-by: Felipe Oliveira dos Santos <felipeoliveira.s.br@gmail.com>

--- pkg/template/v2/decrypt.go
+++ pkg/template/v2/decrypt.go
@@ -5,64 +5,53 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/sha512"
-	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"hash"
 )
 
-const (
-	errParsePK = "could not apply parse of private key type to %v in %v"
+var (
+	errParsePK    = errors.New("could not parse private key")
+	errRSADecrypt = errors.New("error decrypting data with RSA")
 )
 
-func rsaDecrypt(scheme string, hash string, privateKeyFormat string, in string, privateKey string) (string, error) {
+const (
+	errSchemeNotSupported = "decryption scheme %v is not supported"
+	errParseRSAPK         = "could not parse RSA private key"
+	errDecodePEM          = "failed to decode PEM block"
+	errWrap               = "%w: %v"
+)
+
+func rsaDecrypt(scheme string, hash string, in string, privateKey string) (string, error) {
+
 	switch scheme {
 	case "None":
 		return in, nil
 	case "RSA-OAEP":
 
-		rsaPrivateKey, err := parseRsaPrivateKeyDecrypt(privateKeyFormat, privateKey)
+		pemBlock, _ := pem.Decode([]byte(privateKey))
+		if pemBlock == nil {
+			return "", fmt.Errorf(errDecodePEM)
+		}
+
+		parsedPrivateKey, err := parsePrivateKey(pemBlock.Bytes)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf(errWrap, errParsePK, err)
+		}
+
+		rsaPrivateKey, isValid := parsedPrivateKey.(*rsa.PrivateKey)
+		if !isValid {
+			return "", fmt.Errorf(errParseRSAPK)
 		}
 
 		out, err := rsa.DecryptOAEP(getHash(hash), nil, rsaPrivateKey, []byte(in), nil)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf(errWrap, errRSADecrypt, err)
 		}
 		return string(out), nil
 	default:
-		return "", fmt.Errorf("decrypting strategy %v is not supported", scheme)
-	}
-}
-
-func parseRsaPrivateKeyDecrypt(privateKeyFormat string, privateKey string) (*rsa.PrivateKey, error) {
-	pemBlock, _ := pem.Decode([]byte(privateKey))
-	if pemBlock == nil {
-		return nil, fmt.Errorf("failed to decode PEM block")
-	}
-
-	switch privateKeyFormat {
-	case "None":
-		return &rsa.PrivateKey{}, nil
-	case "PKCS8":
-		parsedPrivateKey, err := x509.ParsePKCS8PrivateKey(pemBlock.Bytes)
-		if err != nil {
-			return nil, err
-		}
-		rsaPrivateKey, isValid := parsedPrivateKey.(*rsa.PrivateKey)
-		if !isValid {
-			return nil, fmt.Errorf(errParsePK, "PKCS8", "RSA")
-		}
-		return rsaPrivateKey, nil
-	case "PKCS1":
-		rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
-		if err != nil {
-			return nil, err
-		}
-		return rsaPrivateKey, nil
-	default:
-		return nil, fmt.Errorf("parse private key %v is not supported", privateKeyFormat)
+		return "", fmt.Errorf(errSchemeNotSupported, scheme)
 	}
 }
 
--- pkg/template/v2/kubernetes.go
+++ pkg/template/v2/kubernetes.go
@@ -2,7 +2,6 @@ package template
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"k8s.io/client-go/kubernetes"
@@ -11,30 +10,29 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Error constants for Kubernetes operations
-var (
-	errCreatingConfig    = errors.New("error creating cluster configuration")
-	errCreatingClientset = errors.New("error creating Kubernetes clientset")
-	errFetchingSecret    = errors.New("error fetching secret")
-	errKeyNotFound       = errors.New("key not found in secret")
+const (
+	errCreatingConfig    = "error creating cluster configuration: %v"
+	errCreatingClientset = "error creating Kubernetes clientset: %v"
+	errFetchingSecret    = "error fetching secret: %v"
+	errKeyNotFound       = "key %s not found in secret %s"
 )
 
 func getSecretKey(secretName, namespace, keyName string) (string, error) {
 	restCfg, err := ctrlcfg.GetConfig()
 	if err != nil {
-		return "", fmt.Errorf("%w: %v", errCreatingConfig, err)
+		return "", fmt.Errorf(errCreatingConfig, err)
 	}
 	clientset, err := kubernetes.NewForConfig(restCfg)
 	if err != nil {
-		return "", fmt.Errorf("%w: %v", errCreatingClientset, err)
+		return "", fmt.Errorf(errCreatingClientset, err)
 	}
 	secret, err := clientset.CoreV1().Secrets(namespace).Get(context.Background(), secretName, metav1.GetOptions{})
 	if err != nil {
-		return "", fmt.Errorf("%w: %v", errFetchingSecret, err)
+		return "", fmt.Errorf(errFetchingSecret, err)
 	}
 	val, ok := secret.Data[keyName]
 	if !ok {
-		return "", fmt.Errorf("%w: %q", errKeyNotFound, keyName)
+		return "", fmt.Errorf(errKeyNotFound, keyName, secretName)
 	}
 	return string(val), nil
 }
