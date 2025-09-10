bec743c40e2306e8006075621e36a757ed089ee7
Ian Duffy <iduffy@cloudsmith.io>
2025-09-10 11:21:17 +0100

    fix: update headers
    Signed-off-by: Ian Duffy <iduffy@cloudsmith.io>

--- pkg/generator/cloudsmith/cloudsmith_test.go
+++ pkg/generator/cloudsmith/cloudsmith_test.go
@@ -1,4 +1,6 @@
 /*
+Copyright © 2025 ESO Maintainer Team
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
