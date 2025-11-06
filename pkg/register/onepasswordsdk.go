1bce671b2db17a3b080157efe08bca6abb5cb039
Shimon <Shimon.Darshan@gmail.com>
2025-11-06 15:40:37 +0200

    feat(security): build tags for all the providers to disable them on demand
    Signed-off-by: Shimon Darshan <Shimon.Darshan@gmail.com>

--- pkg/register/onepasswordsdk.go
+++ pkg/register/onepasswordsdk.go
@@ -1,3 +1,5 @@
+//go:build onepasswordsdk || all_providers
+
 /*
 Copyright © 2025 ESO Maintainer Team
 
@@ -19,14 +21,10 @@ package register
 
 import (
 	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
-	fake "github.com/external-secrets/external-secrets/providers/v1/fake"
-	kubernetes "github.com/external-secrets/external-secrets/providers/v1/kubernetes"
-	webhook "github.com/external-secrets/external-secrets/providers/v1/webhook"
+	onepasswordsdk "github.com/external-secrets/external-secrets/providers/v1/onepasswordsdk"
 )
 
 func init() {
-	// Register Kuberntetes providers
-	esv1.Register(fake.NewProvider(), fake.ProviderSpec(), fake.MaintenanceStatus())
-	esv1.Register(kubernetes.NewProvider(), kubernetes.ProviderSpec(), kubernetes.MaintenanceStatus())
-	esv1.Register(webhook.NewProvider(), webhook.ProviderSpec(), webhook.MaintenanceStatus())
+	// Register onepasswordsdk provider
+	esv1.Register(onepasswordsdk.NewProvider(), onepasswordsdk.ProviderSpec(), onepasswordsdk.MaintenanceStatus())
 }
