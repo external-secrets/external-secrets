119f3bffa4c4ffcc64f44889801e43721ba4f703
Gergely Brautigam <skarlso777@gmail.com>
2025-11-17 10:23:15 +0100

    chore: fix sonar cloud issues (#5405)
    Signed-off-by: Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
    
    On-behalf-of: Gergely Brautigam <gergely.brautigam@sap.com>

--- hack/helm.generate.sh
+++ hack/helm.generate.sh
@@ -17,18 +17,18 @@ yq e -Ns "\"${HELM_DIR}/templates/crds/\" + .spec.names.singular" ${BUNDLE_DIR}/
 
 # Add helm if statement for controlling the install of CRDs
 for i in "${HELM_DIR}"/templates/crds/*.yml; do
-  export CRDS_FLAG_NAME="create$(yq e '.spec.names.kind' $i)"
+  export CRDS_FLAG_NAME="create$(yq e '.spec.names.kind' "${i}")"
   cp "$i" "$i.bkp"
-  if [[ "$CRDS_FLAG_NAME" == *"ExternalSecret"* || "$CRDS_FLAG_NAME" == *"SecretStore"* ]]; then
+  if [[ "${CRDS_FLAG_NAME}" == *"ExternalSecret"* || "${CRDS_FLAG_NAME}" == *"SecretStore"* ]]; then
     yq e '(.spec.versions[] | select(.name == "v1alpha1")) |= ("{{- if .Values.crds.conversion.enabled }}\n \(.)\n {{- end }}")' -i "$i.bkp" || true
     $SEDPRG -i '/- |-/d' "$i.bkp"
     # Indent the remaining additionalPrinterColumn property right
     $SEDPRG -i 's/       additionalPrinterColumns:/    - additionalPrinterColumns:/' "$i.bkp"
   fi
 
-  if [[ "$CRDS_FLAG_NAME" == *"Cluster"* ]]; then
+  if [[ "${CRDS_FLAG_NAME}" == *"Cluster"* ]]; then
     echo "{{- if and (.Values.installCRDs) (.Values.crds.$CRDS_FLAG_NAME) }}" > "$i"
-  elif [[ "$CRDS_FLAG_NAME" == *"PushSecret"* ]]; then
+  elif [[ "${CRDS_FLAG_NAME}" == *"PushSecret"* ]]; then
 			echo "{{- if and (.Values.installCRDs) (.Values.crds.$CRDS_FLAG_NAME) }}" > "$i"
   else
     echo "{{- if .Values.installCRDs }}" > "$i"
