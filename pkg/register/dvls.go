77157da5f10b808f56f8e067b6327e1b6b66a623
Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>
2026-02-11 09:33:21 +0100

    Add dvls metadata
    Signed-off-by: Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>

--- pkg/register/dvls.go
+++ pkg/register/dvls.go
@@ -7,7 +7,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	https://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,11 +20,5 @@ limitations under the License.
 package register
 
 import (
-	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
-	dvls "github.com/external-secrets/external-secrets/providers/v1/dvls"
+	_ "github.com/external-secrets/external-secrets/providers/v1/dvls"
 )
-
-func init() {
-	// Register DVLS provider
-	esv1.Register(dvls.NewProvider(), dvls.ProviderSpec(), dvls.MaintenanceStatus())
-}
