99e2fa018467017b1f0c9695338a0cc8a89d81c5
Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>
2026-02-11 23:24:56 +0100

    Fix 1passwordsdk consistency
    Signed-off-by: Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>

--- providers/v1/onepasswordsdk/register.go
+++ providers/v1/onepasswordsdk/register.go
@@ -25,7 +25,7 @@ import (
 
 func init() {
 	esv1.Register(NewProvider(), ProviderSpec(), MaintenanceStatus())
-	provider.Register("onepasswordsdk", Metadata())
+	provider.Register("onepasswordSDK", Metadata())
 }
 
 func Metadata() provider.Metadata {
