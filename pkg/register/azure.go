0802513632ee34167b7693c101084cd6b7dfab30
Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>
2026-02-11 09:45:31 +0100

    Add Azure metadata
    Signed-off-by: Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>

--- pkg/register/azure.go
+++ pkg/register/azure.go
@@ -20,11 +20,5 @@ limitations under the License.
 package register
 
 import (
-	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
-	azure "github.com/external-secrets/external-secrets/providers/v1/azure/keyvault"
+	_ "github.com/external-secrets/external-secrets/providers/v1/azure/keyvault"
 )
-
-func init() {
-	// Register azure provider
-	esv1.Register(azure.NewProvider(), azure.ProviderSpec(), azure.MaintenanceStatus())
-}
