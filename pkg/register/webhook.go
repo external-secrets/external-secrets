8e7f5322eb7c8df79d4b446161ad473344664027
Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>
2026-02-11 09:33:22 +0100

    Add webhook metadata
    Signed-off-by: Jean-Philippe Evrard <jean-philippe.evrard+rochepub@external.roche.com>

--- pkg/register/webhook.go
+++ pkg/register/webhook.go
@@ -20,11 +20,5 @@ limitations under the License.
 package register
 
 import (
-	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
-	webhook "github.com/external-secrets/external-secrets/providers/v1/webhook"
+	_ "github.com/external-secrets/external-secrets/providers/v1/webhook"
 )
-
-func init() {
-	// Register webhook provider
-	esv1.Register(webhook.NewProvider(), webhook.ProviderSpec(), webhook.MaintenanceStatus())
-}
