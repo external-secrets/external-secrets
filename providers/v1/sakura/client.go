f68ef0691c85be64d26b842fbea51e6083d6b03a
Takuto Nagami <logica0419@gmail.com>
2026-02-20 07:45:16 +0900

    implement client interface
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- providers/v1/sakura/client.go
+++ providers/v1/sakura/client.go
@@ -18,6 +18,8 @@ package sakura
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strconv"
 
 	"github.com/sacloud/secretmanager-api-go"
@@ -25,8 +27,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 
 	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
+	"github.com/external-secrets/external-secrets/runtime/esutils"
+	"github.com/external-secrets/external-secrets/runtime/find"
 )
 
+// Client implements the esv1.SecretsClient interface for Sakura Cloud Secret Manager.
 type Client struct {
 	api secretmanager.SecretAPI
 }
@@ -34,60 +39,153 @@ type Client struct {
 // Check if the Client satisfies the esv1.SecretsClient interface.
 var _ esv1.SecretsClient = &Client{}
 
-func (c *Client) GetSecret(ctx context.Context, ref esv1.ExternalSecretDataRemoteRef) ([]byte, error) {
-	version := v1.OptNilInt{}
+// ----------------- Utilities -----------------
 
-	if ref.Version != "" {
-		versionInt, err := strconv.Atoi(ref.Version)
+// Retrieve the value of a secret
+func (c *Client) unveilSecret(ctx context.Context, name string, version string) ([]byte, error) {
+	versionOpt := v1.OptNilInt{}
+	if version != "" {
+		versionInt, err := strconv.Atoi(version)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("invalid version: %w", err)
 		}
 
-		version = v1.NewOptNilInt(versionInt)
+		versionOpt = v1.NewOptNilInt(versionInt)
 	}
 
 	res, err := c.api.Unveil(ctx, v1.Unveil{
-		Name:    ref.Key,
-		Version: version,
+		Name:    name,
+		Version: versionOpt,
 	})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unveil secret: %w", err)
 	}
 
 	return []byte(res.GetValue()), nil
 }
 
+// Check if a secret with the given name exists
+func (c *Client) secretExists(ctx context.Context, name string) (bool, error) {
+	secrets, err := c.api.List(ctx)
+	if err != nil {
+		return false, fmt.Errorf("failed to list secrets: %w", err)
+	}
+
+	for _, secret := range secrets {
+		if secret.Name == name {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// ----------------- Interface implementation -----------------
+
+func (c *Client) GetSecret(ctx context.Context, ref esv1.ExternalSecretDataRemoteRef) ([]byte, error) {
+	return c.unveilSecret(ctx, ref.Key, ref.Version)
+}
+
 func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, data esv1.PushSecretData) error {
-	// Implement the logic to push a secret to Sakura Cloud
+	value, err := esutils.ExtractSecretData(data, secret)
+	if err != nil {
+		return fmt.Errorf("failed to extract secret data: %w", err)
+	}
+
+	// Since Create and Update methods are not distinguished in SecretAPI, simply call Create here
+	// 	ref: https://github.com/sacloud/secretmanager-api-go/blob/main/secrets.go#L65-L68
+	if _, err = c.api.Create(ctx, v1.CreateSecret{
+		Name:  data.GetRemoteKey(),
+		Value: string(value),
+	}); err != nil {
+		return fmt.Errorf("failed to update secret: %w", err)
+	}
+
 	return nil
 }
 
 func (c *Client) DeleteSecret(ctx context.Context, remoteRef esv1.PushSecretRemoteRef) error {
-	// Implement the logic to delete a secret from Sakura Cloud
+	if err := c.api.Delete(ctx, v1.DeleteSecret{
+		Name: remoteRef.GetRemoteKey(),
+	}); err != nil {
+		return fmt.Errorf("failed to delete secret: %w", err)
+	}
+
 	return nil
 }
 
 func (c *Client) SecretExists(ctx context.Context, remoteRef esv1.PushSecretRemoteRef) (bool, error) {
-	// Implement the logic to check if a secret exists in Sakura Cloud
-	return false, nil
+	return c.secretExists(ctx, remoteRef.GetRemoteKey())
 }
 
 func (c *Client) Validate() (esv1.ValidationResult, error) {
-	// Implement the logic to validate the client configuration
-	return esv1.ValidationResultUnknown, nil
+	if _, err := c.api.List(context.Background()); err != nil {
+		return esv1.ValidationResultError, fmt.Errorf("failed to validate client: %w", err)
+	}
+
+	return esv1.ValidationResultReady, nil
 }
 
 func (c *Client) GetSecretMap(ctx context.Context, ref esv1.ExternalSecretDataRemoteRef) (map[string][]byte, error) {
-	// Implement the logic to get a secret map from Sakura Cloud
-	return nil, nil
+	data, err := c.unveilSecret(ctx, ref.Key, ref.Version)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secret: %w", err)
+	}
+
+	kv := make(map[string]json.RawMessage)
+	if err = json.Unmarshal(data, &kv); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal secret %s as JSON: %w", ref.Key, err)
+	}
+
+	secretData := make(map[string][]byte)
+	for k, v := range kv {
+		var strVal string
+		if err = json.Unmarshal(v, &strVal); err == nil {
+			secretData[k] = []byte(strVal)
+		} else {
+			secretData[k] = v
+		}
+	}
+
+	return secretData, nil
 }
 
+// Only Name filter is supported
 func (c *Client) GetAllSecrets(ctx context.Context, ref esv1.ExternalSecretFind) (map[string][]byte, error) {
-	// Implement the logic to get all secrets from Sakura Cloud
-	return nil, nil
+	secrets, err := c.api.List(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list secrets: %w", err)
+	}
+
+	// Create regexp matcher for Name filter
+	var matcher *find.Matcher
+	if ref.Name != nil {
+		m, err := find.New(*ref.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		matcher = m
+	}
+
+	secretMap := make(map[string][]byte)
+	for _, s := range secrets {
+		// Skip unmatched secrets for Name filter
+		if matcher != nil && !matcher.MatchName(s.Name) {
+			continue
+		}
+
+		res, err := c.unveilSecret(ctx, s.Name, "")
+		if err != nil {
+			return nil, fmt.Errorf("failed to unveil secret %s: %w", s.Name, err)
+		}
+
+		secretMap[s.Name] = res
+	}
+
+	return secretMap, nil
 }
 
 func (c *Client) Close(ctx context.Context) error {
-	// Implement the logic to close the client connection
 	return nil
 }
