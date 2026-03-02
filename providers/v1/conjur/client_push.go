b4039ad75f92f8a2611bb80524ecb030dc300755
Adrian Lai <adrian.lai@jetstack.io>
2026-03-02 11:08:05 +0000

    Implement SecretKey/RemoteProperty for Conjur PushSecrets
    Signed-off-by: Adrian Lai <adrian.lai@jetstack.io>

--- providers/v1/conjur/client_push.go
+++ providers/v1/conjur/client_push.go
@@ -28,24 +28,27 @@ import (
 	corev1 "k8s.io/api/core/v1"
 
 	esv1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1"
+	"github.com/external-secrets/external-secrets/runtime/esutils"
 )
 
 const policyTemplate = `
 - !policy
   id: {{ .Name }}
   body:
+{{- range .Variables}}
   - !variable
-    id: {{ .Variable }}
+    id: {{ . }}
+{{- end -}}
 `
 
-func conjurPolicy(name, variable string) string {
+func conjurPolicy(name string, vars []string) string {
 	type policy struct {
-		Name     string
-		Variable string
+		Name      string
+		Variables []string
 	}
 	p := policy{
-		Name:     name,
-		Variable: variable,
+		Name:      name,
+		Variables: vars,
 	}
 	t := template.Must(template.New("policy").Parse(policyTemplate))
 	buf := &bytes.Buffer{}
@@ -60,9 +63,22 @@ func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, ref esv1
 		return getConjurClientError
 	}
 
-	val, ok := secret.Data[ref.GetSecretKey()]
-	if !ok {
-		return errors.New("key not found")
+	values := map[string]string{}
+	vars := []string{}
+
+	key := ref.GetSecretKey()
+	if key == "" {
+		for k, v := range secret.Data {
+			values[k] = string(v)
+			vars = append(vars, k)
+		}
+	} else {
+		value, ok := secret.Data[key]
+		if !ok {
+			return errors.New("key not found")
+		}
+		values[key] = string(value)
+		vars = append(vars, key)
 	}
 
 	fqSecretName := ref.GetRemoteKey()
@@ -72,20 +88,42 @@ func (c *Client) PushSecret(ctx context.Context, secret *corev1.Secret, ref esv1
 	if i == -1 {
 		return errors.New("Expected RemoteKey to contain a '/'")
 	}
-	if secretKey == "" {
-		return errors.New("property required")
+	if secretKey != "" {
+		vars = []string{secretKey}
 	}
 	parentPolicy := fqSecretName[0:i]
 	policyName := fqSecretName[i+1:]
-	policy := conjurPolicy(policyName, secretKey)
+	policy := conjurPolicy(policyName, vars)
 
 	_, err := conjurClient.LoadPolicy(conjurapi.PolicyModePost, parentPolicy, strings.NewReader(policy))
 	if err != nil {
 		return err
 	}
-	err = conjurClient.AddSecret(fmt.Sprintf("%s/%s", fqSecretName, secretKey), string(val))
-	if err != nil {
-		return err
+	// if we're not given a property, store all the secrets under the k8s secret key
+	if secretKey == "" {
+		for k, v := range values {
+			err = conjurClient.AddSecret(fmt.Sprintf("%s/%s", fqSecretName, k), v)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	// if we have a property and a single k8s secret key, store it "as is"
+	if secretKey != "" && key != "" {
+		err = conjurClient.AddSecret(fmt.Sprintf("%s/%s", fqSecretName, secretKey), values[key])
+		if err != nil {
+			return err
+		}
+	} else if secretKey != "" && key == "" {
+		// if we have a property, and all the k8s secret fields, store it as a json obj.
+		value, err := esutils.JSONMarshal(values)
+		if err != nil {
+			return err
+		}
+		err = conjurClient.AddSecret(fmt.Sprintf("%s/%s", fqSecretName, secretKey), string(value))
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
