b9d98cedbf44bd4636b545d610a2e1154763da2f
Moritz Johner <beller.moritz@googlemail.com>
2026-04-11 00:32:04 +0200

    test: expand kubernetes v2 provider coverage
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- e2e/suites/provider/cases/kubernetes/provider_v2.go
+++ e2e/suites/provider/cases/kubernetes/provider_v2.go
@@ -80,4 +80,56 @@ var _ = Describe("[kubernetes] v2 namespaced provider", Label("kubernetes", "v2"
 		_, err := f.WaitForSecretValue(f.Namespace.Name, "provider-v2-target", expected)
 		Expect(err).NotTo(HaveOccurred())
 	})
+
+	It("syncs ExternalSecret dataFrom.find through a namespaced Provider", func() {
+		secretOne := "provider-v2-find-one"
+		secretTwo := "provider-v2-find-two"
+		secretThree := "provider-v2-ignore"
+
+		prov.CreateSecret(secretOne, framework.SecretEntry{
+			Value: `{"value":"provider-v2-one"}`,
+		})
+		prov.CreateSecret(secretTwo, framework.SecretEntry{
+			Value: `{"value":"provider-v2-two"}`,
+		})
+		prov.CreateSecret(secretThree, framework.SecretEntry{
+			Value: `{"value":"provider-v2-ignore"}`,
+		})
+
+		externalSecret := &esv1.ExternalSecret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "provider-v2-find-es",
+				Namespace: f.Namespace.Name,
+			},
+			Spec: esv1.ExternalSecretSpec{
+				RefreshInterval: &metav1.Duration{Duration: defaultV2RefreshInterval},
+				SecretStoreRef: esv1.SecretStoreRef{
+					Name: f.Namespace.Name,
+					Kind: esv1.ProviderKindStr,
+				},
+				Target: esv1.ExternalSecretTarget{
+					Name: "provider-v2-find-target",
+				},
+				DataFrom: []esv1.ExternalSecretDataFromRemoteRef{{
+					Find: &esv1.ExternalSecretFind{
+						Name: &esv1.FindName{
+							RegExp: "provider-v2-find-(one|two)",
+						},
+					},
+				}},
+			},
+		}
+		Expect(f.CRClient.Create(context.Background(), externalSecret)).To(Succeed())
+
+		expected := &corev1.Secret{
+			Type: corev1.SecretTypeOpaque,
+			Data: map[string][]byte{
+				secretOne: []byte(`{"value":"provider-v2-one"}`),
+				secretTwo: []byte(`{"value":"provider-v2-two"}`),
+			},
+		}
+
+		_, err := f.WaitForSecretValue(f.Namespace.Name, "provider-v2-find-target", expected)
+		Expect(err).NotTo(HaveOccurred())
+	})
 })
