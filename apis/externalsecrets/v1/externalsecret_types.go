8c44bbc0a126b031039a3e82d833618b3f33a63f
Andreas Bergmeier <andreas.bergmeier@otto.de>
2026-04-22 16:44:19 +0200

    Add initial Documentation for enabling fetching all available Versions.
    Signed-off-by: Andreas Bergmeier <andreas.bergmeier@otto.de>

--- apis/externalsecrets/v1/externalsecret_types.go
+++ apis/externalsecrets/v1/externalsecret_types.go
@@ -306,6 +306,11 @@ type ExternalSecretDataRemoteRef struct {
 	DecodingStrategy ExternalSecretDecodingStrategy `json:"decodingStrategy,omitempty"`
 
 	// +optional
+	// Used to define a version Strategy
+	// +kubebuilder:default="None"
+	VersionStrategy ExternalSecretVersionStrategy `json:"versionStrategy,omitempty"`
+
+	// +optional
 	// Controls how ESO handles fetched secret data containing NUL bytes for this source.
 	// +kubebuilder:default="Ignore"
 	NullBytePolicy ExternalSecretNullBytePolicy `json:"nullBytePolicy,omitempty"`
@@ -346,6 +351,20 @@ const (
 	ExternalSecretDecodeBase64URL ExternalSecretDecodingStrategy = "Base64URL"
 	// ExternalSecretDecodeNone specifies that no decoding should be performed.
 	ExternalSecretDecodeNone ExternalSecretDecodingStrategy = "None"
+)
+
+// ExternalSecretVersionStrategy defines strategies for handling multiple versions of secrets in the provider.
+// +kubebuilder:validation:Enum=Auto;Available
+type ExternalSecretVersionStrategy string
+
+const (
+	// ExternalSecretVersionAuto specifies that the version strategy should be determined automatically.
+	// This is the default strategy and will fetch the latest version of the secret.
+	ExternalSecretVersionAuto ExternalSecretVersionStrategy = "Auto"
+	// ExternalSecretVersionAvailable specifies that all available versions should be fetched.
+	// Many stores have a concept of soft or hard deleting secret versions.
+	// This strategy will fetch all secret versions that are neither soft nor hard deleted.
+	ExternalSecretVersionAvailable ExternalSecretVersionStrategy = "Available"
 )
 
 // ExternalSecretDataFromRemoteRef defines the connection between the Kubernetes Secret keys and the Provider data
@@ -484,6 +503,11 @@ type ExternalSecretFind struct {
 	// Find secrets based on tags.
 	// +optional
 	Tags map[string]string `json:"tags,omitempty"`
+
+	// +optional
+	// Used to define a version Strategy
+	// +kubebuilder:default="Default"
+	VersionStrategy ExternalSecretVersionStrategy `json:"versionStrategy,omitempty"`
 
 	// +optional
 	// Used to define a conversion Strategy
