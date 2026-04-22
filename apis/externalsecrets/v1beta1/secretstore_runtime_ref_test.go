bbca36e5e0bc866b037be035593a833cad0c8308
Moritz Johner <beller.moritz@googlemail.com>
2026-04-23 00:10:07 +0200

    test: add v1beta1 runtimeRef deepcopy parity
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- apis/externalsecrets/v1beta1/secretstore_runtime_ref_test.go
+++ apis/externalsecrets/v1beta1/secretstore_runtime_ref_test.go
@@ -37,3 +37,23 @@ func TestSecretStoreSpecDeepCopyPreservesProviderClassRuntimeRef(t *testing.T) {
 		t.Fatalf("expected RuntimeRef to be deep-copied")
 	}
 }
+
+func TestSecretStoreSpecDeepCopyPreservesClusterProviderClassRuntimeRef(t *testing.T) {
+	spec := &SecretStoreSpec{
+		RuntimeRef: &StoreRuntimeRef{
+			Kind: "ClusterProviderClass",
+			Name: "aws",
+		},
+	}
+
+	out := spec.DeepCopy()
+	if out.RuntimeRef == nil {
+		t.Fatalf("expected RuntimeRef to be copied")
+	}
+	if out.RuntimeRef.Name != "aws" || out.RuntimeRef.Kind != "ClusterProviderClass" {
+		t.Fatalf("unexpected RuntimeRef copy: %#v", out.RuntimeRef)
+	}
+	if out.RuntimeRef == spec.RuntimeRef {
+		t.Fatalf("expected RuntimeRef to be deep-copied")
+	}
+}
