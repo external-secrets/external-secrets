c0277d99e747547e1481fb39f6e422e4bb30ee38
Charles Moscofian <charlesmoscofian@hotmail.com>
2026-06-10 11:01:29 +0200

    test(aws/sm): add failed to replicate case
    Signed-off-by: Charles Moscofian <charlesmoscofian@hotmail.com>

--- providers/v1/aws/secretsmanager/secretsmanager_test.go
+++ providers/v1/aws/secretsmanager/secretsmanager_test.go
@@ -1366,6 +1366,39 @@ func TestSetSecret(t *testing.T) {
 				err: nil,
 			},
 		},
+		"SetReplicationForInvalidRegionFails": {
+			reason: "sync an existing secret with existing replication region previously setup",
+			args: args{
+				store: makeValidSecretStore().Spec.Provider.AWS,
+				client: fakesm.Client{
+					GetSecretValueFn:           fakesm.NewGetSecretValueFn(secretValueOutput, nil),
+					PutSecretValueFn:           fakesm.NewPutSecretValueFn(putSecretOutput, nil),
+					DescribeSecretFn:           fakesm.NewDescribeSecretFn(tagSecretOutput, nil),
+					DeleteResourcePolicyFn:     fakesm.NewDeleteResourcePolicyFn(nil, &types.ResourceNotFoundException{}),
+					ReplicateSecretToRegionsFn: fakesm.NewReplicateSecretToRegionsFn(nil, &types.InvalidRequestException{}),
+				},
+				pushSecretData: fake.PushSecretData{
+					SecretKey: secretKey,
+					RemoteKey: fakeKey,
+					Property:  "",
+					Metadata: &apiextensionsv1.JSON{
+						Raw: []byte(`{
+							"apiVersion": "kubernetes.external-secrets.io/v1alpha1",
+							"kind": "PushSecretMetadata",
+							"spec": {
+								"secretPushFormat": "string",
+								"replicationLocations": [
+									"xx-invalid-1"
+								]
+							}
+						}`),
+					},
+				},
+			},
+			want: want{
+				err: errors.New("failed to replicate existing secret to regions"),
+			},
+		},
 	}
 
 	for name, tc := range tests {
